57252963dbb4c0fd007395319f42ce9cf803df31
johnitf <hehegsharp@gmail.com>
2018-12-17 14:33:16 +0800

    1217

--- etc/ref1.py
+++ etc/ref1.py
@@ -1,1 +1,2 @@
 # https://www.pyimagesearch.com/2015/02/23/install-opencv-and-python-on-your-raspberry-pi-2-and-b/
+# https://www.pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
